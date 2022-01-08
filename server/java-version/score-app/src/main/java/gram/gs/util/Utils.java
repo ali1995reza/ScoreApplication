fb3d1457c2379d0052b2fc5279e029b9db4b6a47
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2022-01-08 23:35:58 +0330

    ADD : Utils test FIX : Utils.USER_ID_PATTERN

--- server/java-version/score-app/src/main/java/gram/gs/util/Utils.java
+++ server/java-version/score-app/src/main/java/gram/gs/util/Utils.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Utils {
 
-    private final static Pattern USER_ID_PATTERN = Pattern.compile("^([a-zA-Z])+([\\w]{5,})+$");
+    private final static Pattern USER_ID_PATTERN = Pattern.compile("^([0-9a-zA-Z_\\-]{3,})+$");
 
     public static boolean isValidUserId(String userId) {
         if (userId == null) {
