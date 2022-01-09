768bc4f7d131d670c6eb94ec036394e31564f070
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2022-01-09 13:46:34 +0330

    ADD : ScoreApplication logs

--- server/java-version/score-app/src/main/java/gram/gs/ScoreApplication.java
+++ server/java-version/score-app/src/main/java/gram/gs/ScoreApplication.java
@@ -7,11 +7,15 @@ import gram.gs.repository.ScoreRepository;
 import gram.gs.repository.UserRepository;
 import gram.gs.service.AuthenticationService;
 import gram.gs.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ScoreApplication {
+
+    public final static Logger LOGGER = LoggerFactory.getLogger(ScoreApplication.class);
 
     private final UserRepository userRepository;
     private final ScoreRepository scoreRepository;
@@ -24,6 +28,7 @@ public class ScoreApplication {
     }
 
     public String login(final String userId) throws ScoreApplicationException {
+        LOGGER.debug("login user with id {}", userId);
         //first validate user id
         Assert.isTrue(Utils.isValidUserId(userId), InvalidUserIdFormatException::new);
         userRepository.addOrGet(userId);
@@ -32,6 +37,7 @@ public class ScoreApplication {
 
     public RankedScore submitScore(final String token, final String applicationId, final long score) throws ScoreApplicationException {
         final String userId = authenticationService.validateToken(token);
+        LOGGER.debug("submit score {} for application {} by user {}", score, applicationId, userId);
         //check if asserted user is really exists
         Assert.isNotNull(userRepository.get(userId), AuthenticationTokenInvalidException::new);
         //check if user id and application id is valid
@@ -43,6 +49,7 @@ public class ScoreApplication {
     }
 
     public List<RankedScore> getTopScoreList(final String applicationId, final long offset, final long size) throws ScoreApplicationException {
+        LOGGER.debug("get top score list for application {} with offset {} and size {}", applicationId, offset, size);
         //check if application id is valid
         Assert.isTrue(Utils.isValidApplicationId(applicationId), InvalidApplicationIdFormatException::new);
         Assert.isNotNegative(offset, () -> new InvalidParametersException("[offset] parameter must greater equals than 0"));
@@ -51,6 +58,7 @@ public class ScoreApplication {
     }
 
     public List<RankedScore> searchScoreList(final String userId, final String applicationId, final int top, final int bottom) throws ScoreApplicationException {
+        LOGGER.debug("search score list of application {} to find score of user {} - params : top={}, bottom={}", applicationId, userId, top, bottom);
         //check if user id and application id is valid
         Assert.isTrue(Utils.isValidUserId(userId), InvalidUserIdFormatException::new);
         Assert.isTrue(Utils.isValidApplicationId(applicationId), InvalidApplicationIdFormatException::new);
@@ -62,6 +70,7 @@ public class ScoreApplication {
     }
 
     public RankedScore getUserScore(final String userId, final String applicationId) throws ScoreApplicationException {
+        LOGGER.debug("get score for user {} in application {}", userId, applicationId);
         //check if user id and application id is valid
         Assert.isTrue(Utils.isValidUserId(userId), InvalidUserIdFormatException::new);
         Assert.isTrue(Utils.isValidApplicationId(applicationId), InvalidApplicationIdFormatException::new);
