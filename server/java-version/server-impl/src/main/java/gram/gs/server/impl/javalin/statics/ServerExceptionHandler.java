8aee5fe39476e5907cd437b954ed27ccb02c548f
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2022-01-09 14:59:44 +0330

    FIX : logger config ADD : log exception in ServerExceptionHandler

--- server/java-version/server-impl/src/main/java/gram/gs/server/impl/javalin/statics/ServerExceptionHandler.java
+++ server/java-version/server-impl/src/main/java/gram/gs/server/impl/javalin/statics/ServerExceptionHandler.java
@@ -3,10 +3,18 @@ package gram.gs.server.impl.javalin.statics;
 import gram.gs.exceptions.AuthenticationException;
 import gram.gs.exceptions.ScoreApplicationException;
 import gram.gs.exceptions.ScoreNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class ServerExceptionHandler {
+
+    public final static Logger LOGGER = LoggerFactory.getLogger(ServerExceptionHandler.class);
 
     private final static String SERVER_ERROR_ID = "SERVER_ERROR";
 
@@ -16,21 +24,25 @@ public class ServerExceptionHandler {
     private final static int BAD_REQUEST_HTTP_STATUS = 400;
 
     public static ServerExceptionResponse handle(Exception e) {
-        if(e instanceof ScoreApplicationException) {
-            ScoreApplicationException exception = (ScoreApplicationException) e;
+        if (e instanceof ScoreApplicationException) {
             ServerExceptionResponse response = new ServerExceptionResponse();
+            ScoreApplicationException exception = (ScoreApplicationException) e;
             response.body(getBody(exception));
-            if(e instanceof AuthenticationException) {
-                return response.status(UNAUTHORIZED_HTTP_STATUS);
+            if (e instanceof AuthenticationException) {
+                response.status(UNAUTHORIZED_HTTP_STATUS);
             }
-            if(e instanceof ScoreNotFoundException) {
-                return response.status(NOT_FOUND_HTTP_STATUS);
+            if (e instanceof ScoreNotFoundException) {
+                response.status(NOT_FOUND_HTTP_STATUS);
             }
-            return response.status(BAD_REQUEST_HTTP_STATUS);
+            response.status(BAD_REQUEST_HTTP_STATUS);
+            LOGGER.warn("[{}] : \r\n {}", response.getBody().getId(), getStackTrace(e));
+            return response;
         } else {
-            return new ServerExceptionResponse()
-                    .body(getInternalServerExceptionBody())
+            ServerExceptionResponse response = new ServerExceptionResponse();
+            response.body(getInternalServerExceptionBody())
                     .status(INTERNAL_SERVER_ERROR_HTTP_STATUS);
+            LOGGER.error("[{}] : \r\n {}", response.getBody().getId(), getStackTrace(e));
+            return response;
         }
     }
 
@@ -46,5 +58,17 @@ public class ServerExceptionHandler {
                 .id(e.getId())
                 .message(e.getMessage())
                 .traceId(UUID.randomUUID().toString());
+    }
+
+    private static String getStackTrace(Throwable e) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        e.printStackTrace(printStream);
+        printStream.close();
+        try {
+            outputStream.close();
+        } catch (IOException ioException) {
+        }
+        return outputStream.toString(StandardCharsets.UTF_8);
     }
 }
