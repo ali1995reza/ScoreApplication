1b7fa1959e93e0d6cbad95698938d2d8651e5f4c
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2022-01-12 01:28:50 +0330

    FIX : regex for user-id and application-id

--- server/java-version/score-app/src/main/java/gram/gs/util/Utils.java
+++ server/java-version/score-app/src/main/java/gram/gs/util/Utils.java
@@ -4,20 +4,21 @@ import java.util.regex.Pattern;
 
 public class Utils {
 
-    private final static Pattern ID_PATTERN = Pattern.compile("^([0-9a-zA-Z_\\-]{3,})+$");
+    private final static Pattern USER_ID_PATTERN = Pattern.compile("^([0-9a-zA-Z_\\-]){3,}$");
+    private final static Pattern APPLICATION_ID_PATTERN = Pattern.compile("^([0-9a-zA-Z_\\-]){3,}$");
 
     public static boolean isValidUserId(String userId) {
         if (userId == null) {
             return false;
         }
-        return ID_PATTERN.matcher(userId).matches();
+        return USER_ID_PATTERN.matcher(userId).matches();
     }
 
     public static boolean isValidApplicationId(String applicationId) {
         if (applicationId == null) {
             return false;
         }
-        return ID_PATTERN.matcher(applicationId).matches();
+        return APPLICATION_ID_PATTERN.matcher(applicationId).matches();
     }
 
 }
