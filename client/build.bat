3c994ff54a5a7f5f28ea21402675057c9f1ea04e
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2022-01-12 17:24:17 +0330

    FIX : client build tools

--- client/build.bat
+++ client/build.bat
@@ -1,4 +1,8 @@
 @echo off
 cd client-app
 call mvn clean compile assembly:single
+IF %ERRORLEVEL% NEQ 0 ( 
+    cd ..
+    exit 22
+)
 cd ..
--- client/build.sh
+++ client/build.sh
@@ -2,4 +2,8 @@
 
 cd client-app
 mvn clean compile assembly:single
+if [ $? -ne 0 ]; then
+    cd ..
+    exit 22
+fi
 cd ..
